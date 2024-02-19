4a8309535adfb325157aa6f3d832236010adca8e
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-18 23:42:00 -0300

    at5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
-
 import './style.css'
 
+import api from "./services/api";
+
+
 function App() {
+
+  const [input, setInput] = useState('')
+
+  async function handleSearch() {
+    
+    
+    if(input ===''){
+      alert('Preencha um CEP')
+      return
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response)
+
+    }catch{
+      alert("Erro ao buscar");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -11,9 +34,11 @@ function App() {
         <input 
         type="text"
         placeholder="Digite seu cep..." 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
         <FiSearch size={25} color="#fff"/>
         </button>
       </div>
